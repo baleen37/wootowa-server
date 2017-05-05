ea8fd9083f1efac37a52d8b9b76ef802341afdd3
baleen37 <baleen37@gmail.com>
2017-05-06 03:15:50 +0900

    Add config_example.py

---
+++ rabbit-proj/glb/config_example.py
@@ -0,0 +1,11 @@
+SECRET_KEY = 'secret!'
+
+DEBUG = True
+
+RABBIT_DB = 'postgresql://user:password@host:port/db'
+
+#RABBIT_REDIS_HOST = 'rabbit-redis.04pm7t.0001.usw2.cache.amazonaws.com'
+RABBIT_REDIS_HOST = 'localhost'
+RABBIT_REDIS_PORT = '6379'
+RABBIT_REDIS_DB = '10'
+
